3fee6ee0316d95de66c8339466e0ac562e63be81
Дмитрий <dmitriy.course@gmail.com>
2022-10-05 16:18:48 +0300

    Lost file after silly dir rename

---
+++ cmd/client_server/client.go
@@ -0,0 +1,112 @@
+package clientserver
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"math"
+	"net/http"
+	customtypes "sample-choose-ad/cmd/custom_types"
+	req_types "sample-choose-ad/cmd/requests_types"
+	"sort"
+	"time"
+)
+
+func sendRequest(url string, body *io.Reader) (req_types.SuccesResponse, error) {
+	var pResp req_types.SuccesResponse
+
+	c := &http.Client{
+		Timeout: 200 * time.Millisecond,
+	}
+
+	resp, err := c.Post(url, "application/json", *body)
+
+	if err != nil {
+		log.Println(err)
+		eText := fmt.Sprintf("%v\n not responding", url)
+		return pResp, errors.New(eText)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 204 {
+		return pResp, errors.New("No content")
+	}
+
+	b, _ := ioutil.ReadAll(resp.Body)
+
+	err = json.Unmarshal(b, &pResp)
+	if err != nil {
+		log.Println(err)
+	}
+	return pResp, nil
+}
+
+// Create requset body based in incoming reqest `ir` and return
+// `OutgoingRequest` as bytes.Reader from marshaled JSON
+func constructPartnersRequestBody(ir *req_types.IncomingRequest) io.Reader {
+	var outReqBody req_types.OutgoingRequest
+
+	var imps []req_types.Imp
+
+	// WARN: uint and float multiplication may cause problems
+	for _, tile := range ir.Tiles {
+		imps = append(imps, req_types.Imp{
+			Id:        tile.Id,
+			Minwidth:  tile.Width,
+			Minheight: uint(math.Floor(float64(tile.Width * uint(tile.Ratio))))})
+	}
+
+	outReqBody.Id = *ir.Id
+	outReqBody.Imp = imps
+	outReqBody.Context = ir.Context
+	t, _ := json.Marshal(outReqBody)
+	return bytes.NewReader(t)
+}
+
+// map[imp.id]map[imp.id.price]
+type PartnersResponses map[uint]map[float64]req_types.RespImp
+
+// Make request for each partner and returns
+func makePartnersRequests(partners []customtypes.PartnersAddress, ir *req_types.IncomingRequest) {
+	p_body := constructPartnersRequestBody(ir)
+
+	// Two data structures:
+	// partnersRespones for getting price with O(1) complexity
+	// []prices as slice of actual prices
+	partnersRespones := make(map[uint]map[float64]req_types.RespImp)
+	prices := make(map[uint][]float64)
+
+	for _, p := range partners {
+		url := fmt.Sprintf("http://%v:%v/%v", p.Ip, p.Port, PARTNER_ENDPOINT)
+
+		re, err := sendRequest(url, &p_body)
+
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		// append only successful responses
+		for _, r := range re.Imp {
+			if partnersRespones[r.Id] == nil {
+				partnersRespones[r.Id] = make(map[float64]req_types.RespImp)
+			}
+			partnersRespones[r.Id][r.Price] = r
+			prices[r.Id] = append(prices[r.Id], r.Price)
+		}
+
+	}
+
+	if len(partnersRespones) == 0 {
+		log.Println("Error: no responses from partners.")
+		return
+	}
+
+	// Sorting prices, now biggest price at index len-1
+	for _, p := range prices {
+		sort.Float64s(p)
+	}
+}
---
+++ cmd/client_server/validators.go
@@ -0,0 +1,51 @@
+package clientserver
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+const MAX_PORT_NUM = 65535
+
+// Returns false if ipv4 `correct`.
+func wrongIPAddresFormat(ipv4 string) bool {
+	re, err := regexp.Compile(`^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$`)
+	if err != nil {
+		log.Println(err)
+	}
+	return !re.Match([]byte(ipv4))
+}
+
+func throwHTTPError(err_text string, code int, w *http.ResponseWriter) error {
+	http.Error(*w, err_text, code)
+	eText := fmt.Sprintf("Error: %d %vr", code, err_text)
+	return errors.New(eText)
+}
+
+// Wait string in format "10.10.10.10:8080", where `10.10.10.10` IPv4,
+// and `8080` port. If ip or port has wrong format, returns error.
+func ParsePartnersAddress(ipAndPort string) (string, int64, error) {
+	var err error
+	iap := strings.Split(ipAndPort, ":")
+
+	ip := iap[0]
+	if wrongIPAddresFormat(ip) {
+		err = errors.New(fmt.Sprintf("Wrong ip address format in partner ip: %v", ip))
+	}
+
+	port, e := strconv.ParseInt(iap[1], 10, 32)
+	if e != nil {
+		err = errors.New(fmt.Sprintf("Wrong port format in partner ip: %v", e))
+	}
+
+	if port > MAX_PORT_NUM {
+		err = errors.New(fmt.Sprintf("Wrong port in partner ip: grater than %v", MAX_PORT_NUM))
+	}
+
+	return ip, port, err
+}
