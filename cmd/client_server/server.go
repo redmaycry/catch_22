35042b859a41f6f33d5dc0ee922814023035a34a
Дмитрий <dmitriy.course@gmail.com>
2022-10-20 16:20:49 +0300

    Timeout for whole handler

--- cmd/client_server/server.go
+++ cmd/client_server/server.go
@@ -5,11 +5,43 @@ import (
 	"log"
 	"net/http"
 	customtypes "sample-choose-ad/cmd/custom_types"
+	"time"
 )
 
-func StartServer(port string, partners []customtypes.PartnersAddress) {
+type customHandler struct {
+	Parners []customtypes.PartnersAddress
+	// context?
+}
 
-	http.HandleFunc("/placements/request", handleRequest(partners))
+func (c *customHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	base := r.URL.Path
+
+	switch base {
+	case "/placements/request":
+
+	default:
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
+func newCustomHandler() *customHandler {
+	return &customHandler{}
+}
+
+func StartServer(port string, partners []customtypes.PartnersAddress) {
+	// mux := http.NewServeMux()
+	// mux.HandleFunc("/placements/request", handleRequest(partners))
+
+	// s := &http.Server{
+	// 	ReadTimeout:  time.Duration(time.Millisecond * 20),
+	// 	WriteTimeout: time.Duration(time.Millisecond * 20),
+	// 	Handler:      newCustomHandler(),
+	// }
+	// s.ListenAndServe()
+	// h := http.HandleFunc("/placements/request", handleRequest(partners))
+	h := http.TimeoutHandler(handleRequest(partners), time.Duration(240*time.Millisecond), "{}")
+	http.Handle("/placements/request", h)
 	// http.HandleFunc("/placements/request", decorate(test2))
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
